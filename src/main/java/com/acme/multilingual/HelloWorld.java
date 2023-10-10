1edfd35e323f893d21abd7695afd827475b80df4
vinayk215 <125876987+vinayk215@users.noreply.github.com>
2023-10-10 12:58:47 +0530

    Update HelloWorld.java

--- src/main/java/com/acme/multilingual/HelloWorld.java
+++ src/main/java/com/acme/multilingual/HelloWorld.java
@@ -6,24 +6,4 @@ public class HelloWorld {
     System.out.println("Hello World!");
   }
 
-  int i=0;
-
-  
-
-    void send(){
-
-
-
-    }
-
-    void rev(){
-
-        System.out.println("Hello");
-
-    }
-
-  void notCovered() {
-    System.out.println("This method is not covered by unit tests");
-  }
-
 }
